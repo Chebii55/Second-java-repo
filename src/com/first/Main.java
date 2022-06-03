5f0e1a7a1f9a77f345d32910afe96f3f746b450a
Chebii55 <shillerchier055@gmail.com>
2022-06-03 14:51:36 +0300

    Second java project

---
+++ src/com/first/Main.java
@@ -0,0 +1,9 @@
+package com.first;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SearchImpl one = new SearchImpl();
+        one.implement();
+    }
+}
---
+++ src/com/first/SearchImpl.java
@@ -0,0 +1,7 @@
+package com.first;
+
+public class SearchImpl {
+    public void implement(){
+        System.out.print("This is an implementation");
+    }
+}
